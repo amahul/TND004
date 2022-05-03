0e8186601729194edbabc19f9ecbfc3ac4b01a1c
aannajonssonn <71869061+aannajonssonn@users.noreply.github.com>
2022-05-03 10:46:35 +0200

    Add files via upload

--- lab3/code/frequency_table.cpp
+++ lab3/code/frequency_table.cpp
@@ -19,11 +19,17 @@
 struct Row {
 	// ADD CODE: exercise 3
 
-	/*// default constructor
-	Row() : counter{ 1 } {};
+	std::string key;
+	int counter = 0;
+
+	// default constructor
+	Row() : counter{ 0 } {};
+
 	// constructor
 	Row(std::string word) : key{ word }, counter{ 1 }{};
-	*/
+	
+	// default destructor
+	~Row() = default;
 
 	// Operator<, sort alphabetically
 	bool operator<(const Row& row) const {
@@ -31,28 +37,11 @@ struct Row {
 	}
 
 	// Operator ==, check if both vectors are equal
-	bool operator==(const Row& a) {
-		return key == a.key;
-	}
-
-	/*void increase_counter() {
-		counter++;
-	}*/
-
-	/*void print() const {
-		std::cout << std::left << std::setw(25) << key << counter << std::endl;
-	}*/
-
-	/*std::string get_key() {
-		return key;
-	}*/
-
-//:private
-	std::string key = "";
-	int counter = 1;
+	bool operator==(const Row& a) const {
+		return key == a.key && counter == a.counter;
+	}	
 
 };
-
 
 // Add other stuff, if needed
 
@@ -64,124 +53,105 @@ bool isPunc(const char& c) {
 
 }
 
-// Varför behöver vi denna? 
-std::ostream& operator<<(std::ostream& out, const Row& r)
-{
-	return out << std::left << std::setw(25) << r.key << r.counter << "\n";
-
-}
+// To control vector
+//std::ostream& operator<<(std::ostream& os, const Row& r) {
+//	return os << std::left << std::setw(25) << r.key << r.counter << "\n";
+//}
 
 #endif
-
 
 
 void exercise3() {
 #ifdef TEST_EXERCISE3
 	{
 		std::ifstream file("../code/text.txt");  // contains 56 unique words
-		// std::ifstream file("../code/text_long.txt"); // contains 497 unique words
+		//std::ifstream file("../code/text_long.txt"); // contains 497 unique words
 
 		if (!file) {
-			std::cout << "Couldn't open file text.txt\n";
-			// std::cout << "Couldn't open file text_long.txt\n";
+			//std::cout << "Couldn't open file text.txt\n";
+			 std::cout << "Couldn't open file text_long.txt\n";
 			return;
 		}
-
-		// Empty frequency table
-		//BinarySearchTree<Row> freq_table;
-
-		/*std::vector<std::string> V{std::istream_iterator<std::string>{file},
-								   std::istream_iterator<std::string>{} };*/
-
-		// Check that tree is empty
-		//std::cout << "Is the tree empty? ";
-		//assert(freq_table.get_count_nodes() == 0);
-		//std::cout << "Yes!\n\n";
-
-		//std::string inWord;
-		//Row new_row;
 
 		int total_counter = 0;
 
 		std::string inWord;
 		BinarySearchTree<Row> Tree;
-		std::vector<Row> V;
+		Row new_row;
 
 		// While we find a new word in in_File
 		while (file >> inWord)
 		{
-			//std::cout << inWord << std::endl;
 			// Remove special characters
 			inWord.erase(std::remove_if(inWord.begin(), inWord.end(), isPunc), inWord.end());
 
 			// Set word to lowercase
 			std::transform(inWord.begin(), inWord.end(), inWord.begin(), std::tolower);
 
-			Row p;
-			p.key = inWord;
-			BinarySearchTree<Row>::Iterator findWord = Tree.find(p);
+			BinarySearchTree<Row>::Iterator findWord = Tree.find(inWord);
 
 			if (findWord != Tree.end()) {
 				findWord->counter++;
 			} 
 			else {
-				Tree.insert(p);
+				Tree.insert(inWord);
+				findWord = Tree.begin();
 			}
 
-			// Copy tree into a vector V
-			std::copy(Tree.begin(), Tree.end(), std::back_inserter(V));
-
-			//Row new_row = Row(inWord);
-			//if (freq_table.contains(new_row)) {
-			//	//increase counter if word already exists
-			//	new_row.increase_counter();
-			//}
-			//else {
-			//	freq_table.insert(new_row);
-			//}
-
 			++total_counter;
+
 		}
 		file.close();
 
-		std::cout << "---------Tree: -----------\n";
-		Tree.printTree();
+		// Insert to vector
+		std::vector<Row> V;
+		std::copy(Tree.begin(), Tree.end(), std::back_inserter(V));
 
-		std::cout << "\n\n\n------ Table: ----------\n";
+		// Make vector of Facit-file
+		std::vector<Row> facit;
+		std::string fWord;
+		int fCount;
+
+		std::ifstream in("../code/frequency_table.txt");
+		//std::ifstream in("../code/frequency_table_long.txt");
+
+		while (in >> fWord >> fCount) {
+			Row pFacit;
+			pFacit.key = fWord;
+			pFacit.counter = fCount;
+			facit.push_back(pFacit);
+		}
+
+		// Print table
+		/*std::cout << "\n\n\n------ Table: ----------\n";
 		std::vector<Row>::iterator r;
-		//for (p = V.begin(); p != V.end(); p++) {
-		//	std::cout << std::left << std::setw(25) << p->key << p->counter << std::endl;
-		//}
-
-		//std::vector<Row>::iterator p;
 		for (r = V.begin(); r != V.end(); ++r) {
 			std::cout << std::left << std::setw(25) << r->key << r->counter << "\n";
-		}
+		}*/
 
-		//std::cout << freq_table.get_count_nodes();
-		/*std::cout << "total_counter " << total_counter << "\n";
+		std::cout << "\n\n" << "Number of words in the file = " << total_counter << "\n";
+		std::cout << "Number of unique words in the file = " << Tree.get_count_nodes() << "\n\n";;
 
-		assert(total_counter == 69);*/
-	
-		std::ofstream myfile("../code/AAA.txt");
-		if (myfile.is_open())
+
+		// Print vector to file to check if it's ok
+		/*std::ofstream testA("../code/AAA.txt");
+		std::ofstream testB("../code/BBB.txt");
+		if (testA.is_open() && testB.is_open())
 		{
-			
-			myfile << "V.size() " << V.size() << '\n';
-			myfile << "---VECTOR---" << '\n';
-			for (int i = 0; i < V.size(); i++) {
-				myfile << V[i] << '\n';
+			for (int i = 0; i < facit.size(); i++) {
+				testA << facit[i];
+				testB << V[i];
 			}
-			myfile << "---END---" << '\n';
-
-			myfile.close();
 		}
-		else std::cout << "Unable to open AAA";
+		else std::cout << "Unable to open file";*/
 
+		// Compare facit-vector and our vector
+		std::cout << "Check with facit if our vector is correct. " << "\n\n\n";;
+		assert(V == facit);
+	} 
 
-		
-	}
-
+	//assert(BinarySearchTree<Row>::get_count_nodes() == 0);
+	
 	std::cout << "Success!!\n";
 #endif
 }
